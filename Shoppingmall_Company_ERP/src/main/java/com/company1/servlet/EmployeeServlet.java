5a86ceade7bd1e72942b5c0407610ec5a0b0f886
hanwul <hanwul@abc.com>
2025-08-20 15:30:42 +0900

    동기화( 한울 15:30수요일 )

--- Shoppingmall_Company_ERP/src/main/java/com/company1/servlet/EmployeeServlet.java
+++ Shoppingmall_Company_ERP/src/main/java/com/company1/servlet/EmployeeServlet.java
@@ -1,85 +1,102 @@
-//package com.company1.controller;
-//
-//import java.io.IOException;
-//import java.util.List;
-//
-//import javax.servlet.RequestDispatcher;
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//// DTO와 DAO의 패키지 경로를 보내주신 코드에 맞게 수정했습니다.
-//// (나중에 com.company1.* 로 변경하시는 것을 추천합니다!)
-//import java.com.company1.dao.EmployeeDAO;
-//import java.com.company1.dto.EmployeeDTO;
-//
-///**
-// * 직원(Employee) 관련 모든 요청을 처리하는 프론트 컨트롤러 서블릿입니다.
-// * URL 매핑: /employee
-// */
-//@WebServlet("/employee")
-//public class EmployeeServlet extends HttpServlet {
-//	private static final long serialVersionUID = 1L;
-//       
-//    public EmployeeServlet() {
-//        super();
-//    }
-//
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		actionDo(request, response);
-//	}
-//
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		request.setCharacterEncoding("UTF-8");
-//		actionDo(request, response);
-//	}
-//	
-//	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		
-//		String command = request.getParameter("command");
-//		
-//		// [수정된 부분 1] new 키워드를 사용해서 DAO 객체를 직접 생성합니다.
-//		EmployeeDAO employeeDAO = new EmployeeDAO();
-//		
-//		String forwardPath = "";
-//		
-//		
-//		if (command == null || command.equals("list")) {
-//			
-//			// [수정된 부분 2] DAO에 작성된 메소드 이름(selectAllEmployees)과 동일하게 변경했습니다.
-//			List<EmployeeDTO> employeeList = employeeDAO.selectAllEmployees();
-//			
-//			request.setAttribute("employeeList", employeeList);
-//			
-//			forwardPath = "/employee/employee_list.jsp";
-//			
-//		} else if (command.equals("add")) {
-//			// (이 부분은 나중에 employee_form.jsp와 함께 구현할 예정입니다.)
-//			// DAO의 insertEmployee() 메소드를 호출하여 DB에 저장합니다.
-//			
-//			System.out.println("직원 등록 요청 처리");
-//			response.sendRedirect("employee?command=list");
-//			return; 
-//			
-//		} else if (command.equals("editForm")) {
-//			// (이 부분도 나중에 구현할 예정입니다.)
-//			
-//			System.out.println("직원 수정 폼 요청 처리");
-//			forwardPath = "/employee/employee_form.jsp";
-//			
-//		} else if (command.equals("delete")) {
-//			// (이 부분도 나중에 구현할 예정입니다.)
-//			
-//			System.out.println("직원 삭제 요청 처리");
-//			response.sendRedirect("employee?command=list");
-//			return;
-//		}
-//		
-//		
-//		RequestDispatcher dispatcher = request.getRequestDispatcher(forwardPath);
-//		dispatcher.forward(request, response);
-//	}
-//
-//}
+package com.company1.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.company1.dao.EmployeeDAO;
+import com.company1.dto.EmployeeDTO;
+
+/**
+ * 직원(Employee) 관련 모든 요청을 처리하는 프론트 컨트롤러 서블릿입니다.
+ * URL 매핑: /employee
+ */
+@WebServlet("/employee")
+public class EmployeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public EmployeeServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDo(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		actionDo(request, response);
+	}
+	
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String command = request.getParameter("command");
+		String action = request.getParameter("action");
+		
+		EmployeeDAO employeeDAO = new EmployeeDAO();
+		
+		String forwardPath = "";
+		
+		
+		if (command == null || command.equals("list")) {
+			// 직원 목록 조회
+			List<EmployeeDTO> employeeList = employeeDAO.selectAllEmployees();
+			request.setAttribute("employeeList", employeeList);
+			forwardPath = "/employee/employee_list.jsp";
+			
+		} else if (command.equals("form")) {
+			// 직원 등록/수정 폼으로 이동
+			String empId = request.getParameter("empId");
+			if (empId != null && !empId.isEmpty()) {
+				// 수정 모드: 해당 직원 정보를 가져와서 폼에 전달
+				// TODO: selectEmployeeById 메서드 구현 후 사용
+				// EmployeeDTO emp = employeeDAO.selectEmployeeById(empId);
+				// request.setAttribute("employee", emp);
+			}
+			forwardPath = "/employee/employee_form.jsp";
+			
+		} else if (action != null && action.equals("insert")) {
+			// 직원 등록 처리
+			String empId = request.getParameter("empId");
+			String empPw = request.getParameter("empPw");
+			String empName = request.getParameter("empName");
+			String position = request.getParameter("position");
+			String auth = request.getParameter("auth");
+			
+			EmployeeDTO empDTO = new EmployeeDTO();
+			empDTO.setEmpId(empId);
+			empDTO.setEmpPw(empPw);
+			empDTO.setEmpName(empName);
+			empDTO.setPosition(position);
+			empDTO.setAuth(auth);
+			
+			employeeDAO.insertEmployee(empDTO);
+			
+			response.sendRedirect("employee?command=list");
+			return; 
+			
+		} else if (action != null && action.equals("update")) {
+			// 직원 수정 처리 (나중에 구현)
+			System.out.println("직원 수정 처리");
+			response.sendRedirect("employee?command=list");
+			return;
+			
+		} else if (command.equals("delete")) {
+			// 직원 삭제 처리 (나중에 구현)
+			System.out.println("직원 삭제 요청 처리");
+			response.sendRedirect("employee?command=list");
+			return;
+		}
+		
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(forwardPath);
+		dispatcher.forward(request, response);
+	}
+
+}
--- Shoppingmall_Company_ERP/src/main/webapp/employee_form.jsp
+++ Shoppingmall_Company_ERP/src/main/webapp/employee_form.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ page import="com.company1.dao.EmployeeDAO" %>
-<%@ page import="com.company1.dto.EmployeeDTO" %>
+<%@ page import="com.company1.dao.EmployeeDAO"%>
+<%@ page import="com.company1.dto.EmployeeDTO"%>
 <%
 	// --- 1. 페이지 모드(등록/수정)를 결정하는 Java코드 ---
 	String empId = request.getParameter("empId"); // URL에 empId가 있는지 확인
